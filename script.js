d16ce336f5bdd3d0e0e8715d41ac3c045fa52a14
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-04-10 11:19:52 +0700

    'Funny Circle'

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ for (let i = 0; i < n; i++) {
         x : randomXCoordinate(),
         y : randomYCoordinate(),
         radius : radiusValue,
-        dx : randomVelocity(),
-        dy : randomVelocity(),
+        vx : randomVelocity(),
+        vy : randomVelocity(),
         color : getRandomColor(),
         mass : getMass(radiusValue),
     };
@@ -75,38 +75,47 @@ function moveCircles() {
     pen.clearRect(0, 0, maxWidth, maxHeight);
     for (let circle of circles) {
         if (circle.x > (maxWidth - circle.radius) || circle.x < circle.radius) {
-            circle.dx = -1 * (circle.dx);
+            circle.vx = -1 * (circle.vx);
         }
         if (circle.y > (maxHeight - circle.radius) || circle.y < circle.radius) {
-            circle.dy = -1 * (circle.dy);
+            circle.vy = -1 * (circle.vy);
         }
-        // for (let i=0; i<n;i++){
-        //     for (let j=i+1;j<n;j++){
-        //         if (Math.sqrt(Math.pow((circles[i].x-circles[j].x),2)
-        //             + Math.pow((circles[i].y-circles[j].y),2)) <= circles[i].radius+circles[j].radius){
-        //             circles[i].vx = -circles[i].vx;
-        //             circles[i].vy = -circles[i].vy;
-        //             circles[j].vx = -circles[j].vx;
-        //             circles[j].vy = -circles[j].vy;
-        //
-        //             circles[i].dx = (circles[i].dx * (circles[i].mass - circles[j].mass) + (2 * circles[j].mass * circles[j].dx)) / (circles[i].mass + circles[j].mass);
-        //             circles[i].dy = (circles[i].dy * (circles[i].mass - circles[j].mass) + (2 * circles[j].mass * circles[j].dy)) / (circles[i].mass + circles[j].mass);
-        //             circles[j].dx = (circles[j].dx * (circles[j].mass - circles[i].mass) + (2 * circles[i].mass * circles[i].dx)) / (circles[i].mass + circles[j].mass);
-        //             circles[j].dy = (circles[j].dy * (circles[j].mass - circles[i].mass) + (2 * circles[i].mass * circles[i].dy)) / (circles[i].mass + circles[j].mass);
-        //
-        //             circles[i].color = getRandomColor();
-        //             circles[j].color = getRandomColor();
-        //         }
-        //     }
-        // }
+        for (let i=0; i<n;i++){
+            for (let j=i+1;j<n;j++){
+                if (Math.sqrt(Math.pow((circles[i].x-circles[j].x),2)
+                    + Math.pow((circles[i].y-circles[j].y),2)) <= circles[i].radius+circles[j].radius){
+                    dx = circles[i].x-circles[j].x;
+                    dy = circles[i].y-circles[j].y;
+                    collisionision_angle = Math.atan2(dy, dx);
+                    magnitude_1 = Math.sqrt(circles[i].vx*circles[i].vx+circles[i].vy*circles[i].vy);
+                    magnitude_2 = Math.sqrt(circles[j].vx*circles[j].vx+circles[j].vy*circles[j].vy);
+                    direction_1 = Math.atan2(circles[i].vy, circles[i].vx);
+                    direction_2 = Math.atan2(circles[j].vy, circles[j].vx);
+                    new_vx_1 = magnitude_1*Math.cos(direction_1-collisionision_angle);
+                    new_vy_1 = magnitude_1*Math.sin(direction_1-collisionision_angle);
+                    new_vx_2 = magnitude_2*Math.cos(direction_2-collisionision_angle);
+                    new_vy_2 = magnitude_2*Math.sin(direction_2-collisionision_angle);
+                    final_vx_1 = ((circles[i].mass-circles[j].mass)*new_vx_1+(circles[j].mass+circles[j].mass)*new_vx_2)/(circles[i].mass+circles[j].mass);
+                    final_vx_2 = ((circles[i].mass+circles[i].mass)*new_vx_1+(circles[j].mass-circles[i].mass)*new_vx_2)/(circles[i].mass+circles[j].mass);
+                    final_vy_1 = new_vy_1;
+                    final_vy_2 = new_vy_2;
+                    circles[i].vx = Math.cos(collisionision_angle)*final_vx_1+Math.cos(collisionision_angle+Math.PI/2)*final_vy_1;
+                    circles[i].vy = Math.sin(collisionision_angle)*final_vx_1+Math.sin(collisionision_angle+Math.PI/2)*final_vy_1;
+                    circles[j].vx = Math.cos(collisionision_angle)*final_vx_2+Math.cos(collisionision_angle+Math.PI/2)*final_vy_2;
+                    circles[j].vy = Math.sin(collisionision_angle)*final_vx_2+Math.sin(collisionision_angle+Math.PI/2)*final_vy_2;
+                    circles[i].color = getRandomColor();
+                    circles[j].color = getRandomColor();
+                }
+            }
+        }
         pen.beginPath();
         pen.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
         pen.fillStyle = circle.color;
         pen.fill();
         pen.stroke();
 
-        circle.x += circle.dx;
-        circle.y += circle.dy;
+        circle.x += circle.vx;
+        circle.y += circle.vy;
     }
 }
 setInterval(moveCircles,10);
